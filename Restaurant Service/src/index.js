1f8cf413bc41cd95a4b77d6137028e47b62ce3f3
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-15 19:35:47 +0530

    Updated index.js

--- Restaurant Service/src/index.js
+++ Restaurant Service/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/index.js');
 const {PORT} = require('./config/serverConfig.js');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
