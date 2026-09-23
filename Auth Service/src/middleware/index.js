235aac19126bab64020a87dccd8db2d3631a0182
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-23 23:45:04 +0530

    Added middleware for Auth-Validation

---
+++ Auth Service/src/middleware/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    requestAuthValidator: require('./auth-request-validators')
+}
--- Auth Service/src/routes/v1/index.js
+++ Auth Service/src/routes/v1/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const {userController} = require('../../controller/index');
+const {requestAuthValidator} = require('../../middleware/index');
 
 const router = express.Router();
 
 // router.post('/user', create);
-router.delete('/user/:id', userController.destroy);
-router.patch('/user/:id', userController.update);
-router.post('/user', userController.signUp);
-router.post('/user/login', userController.login);
+router.delete('/user/:id', requestAuthValidator.validateUserAuth, userController.destroy);
+router.patch('/user/:id', requestAuthValidator.validateUserAuth, userController.update);
+router.post('/user', requestAuthValidator.validateUserAuth, userController.signUp);
+router.post('/user/login', requestAuthValidator.validateUserAuth, userController.login);
 
 router.get('/isAuthenticated', userController.isAuthenticated);
 
-router.get('/isAdmin', userController.isAdmin);
+router.get('/isAdmin', requestAuthValidator.validateIsAdminRequest, userController.isAdmin);
 
 module.exports = router;
